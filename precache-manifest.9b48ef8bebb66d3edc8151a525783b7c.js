0cbd55882b098ae0793b12dcb84cdd332d0c491b
xingxiaodong1994 <1204642219@qq.com>
2018-10-31 16:22:21 +0800

    Updates

---
+++ precache-manifest.9b48ef8bebb66d3edc8151a525783b7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c843a388396449a01f2",
+    "url": "/drawing-board/build/static/js/runtime~main.5c843a38.js"
+  },
+  {
+    "revision": "c71684ca0d7ed6f79f89",
+    "url": "/drawing-board/build/static/js/main.c71684ca.chunk.js"
+  },
+  {
+    "revision": "defed9a41435a1e9372e",
+    "url": "/drawing-board/build/static/js/1.defed9a4.chunk.js"
+  },
+  {
+    "revision": "c71684ca0d7ed6f79f89",
+    "url": "/drawing-board/build/static/css/main.a180cf39.chunk.css"
+  },
+  {
+    "revision": "19d049824b91c537c400598e5f80e690",
+    "url": "/drawing-board/build/index.html"
+  }
+];
